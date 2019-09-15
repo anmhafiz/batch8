9e2d49f0bdf42bdc6396c936ceebe8d04cd928b8
Abu Naser Mohammad Hafizur Rahman <anmhafiz@gmail.com>
2019-09-15 22:12:24 +0600

    Assignment 2

---
+++ Assignment2/WindowsFormsApp5/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace WindowsFormsApp5
+{
+    partial class coffeShop
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxContract = new System.Windows.Forms.TextBox();
+            this.textBoxAddress = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBoxItem = new System.Windows.Forms.ComboBox();
+            this.textBoxQuantity = new System.Windows.Forms.TextBox();
+            this.richTextBoxShow = new System.Windows.Forms.RichTextBox();
+            this.show = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(80, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Customer Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(80, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Contact no";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(80, 180);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Address";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(231, 97);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(100, 20);
+            this.textBoxName.TabIndex = 3;
+            // 
+            // textBoxContract
+            // 
+            this.textBoxContract.Location = new System.Drawing.Point(231, 138);
+            this.textBoxContract.Name = "textBoxContract";
+            this.textBoxContract.Size = new System.Drawing.Size(100, 20);
+            this.textBoxContract.TabIndex = 4;
+            // 
+            // textBoxAddress
+            // 
+            this.textBoxAddress.Location = new System.Drawing.Point(231, 173);
+            this.textBoxAddress.Name = "textBoxAddress";
+            this.textBoxAddress.Size = new System.Drawing.Size(100, 20);
+            this.textBoxAddress.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(83, 57);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Customer Information";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(83, 243);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Order";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(80, 284);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Quantithy";
+            // 
+            // comboBoxItem
+            // 
+            this.comboBoxItem.FormattingEnabled = true;
+            this.comboBoxItem.Items.AddRange(new object[] {
+            "Math",
+            "English",
+            "Bangla ",
+            "Art"});
+            this.comboBoxItem.Location = new System.Drawing.Point(221, 225);
+            this.comboBoxItem.Name = "comboBoxItem";
+            this.comboBoxItem.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxItem.TabIndex = 9;
+            this.comboBoxItem.SelectedIndexChanged += new System.EventHandler(this.comboBoxItem_SelectedIndexChanged);
+            // 
+            // textBoxQuantity
+            // 
+            this.textBoxQuantity.Location = new System.Drawing.Point(231, 284);
+            this.textBoxQuantity.Name = "textBoxQuantity";
+            this.textBoxQuantity.Size = new System.Drawing.Size(100, 20);
+            this.textBoxQuantity.TabIndex = 10;
+            // 
+            // richTextBoxShow
+            // 
+            this.richTextBoxShow.Location = new System.Drawing.Point(538, 87);
+            this.richTextBoxShow.Name = "richTextBoxShow";
+            this.richTextBoxShow.Size = new System.Drawing.Size(189, 252);
+            this.richTextBoxShow.TabIndex = 11;
+            this.richTextBoxShow.Text = "";
+            // 
+            // show
+            // 
+            this.show.Location = new System.Drawing.Point(268, 357);
+            this.show.Name = "show";
+            this.show.Size = new System.Drawing.Size(75, 23);
+            this.show.TabIndex = 12;
+            this.show.Text = "Save";
+            this.show.UseVisualStyleBackColor = true;
+            this.show.Click += new System.EventHandler(this.show_Click);
+            // 
+            // coffeShop
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.show);
+            this.Controls.Add(this.richTextBoxShow);
+            this.Controls.Add(this.textBoxQuantity);
+            this.Controls.Add(this.comboBoxItem);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxAddress);
+            this.Controls.Add(this.textBoxContract);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "coffeShop";
+            this.Text = "Coffe Shop";
+            this.Load += new System.EventHandler(this.coffeShop_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxContract;
+        private System.Windows.Forms.TextBox textBoxAddress;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboBoxItem;
+        private System.Windows.Forms.TextBox textBoxQuantity;
+        private System.Windows.Forms.RichTextBox richTextBoxShow;
+        private System.Windows.Forms.Button show;
+    }
+}
+
---
+++ Assignment2/WindowsFormsApp5/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    public partial class coffeShop : Form
+    {
+        public coffeShop()
+        {
+            InitializeComponent();
+        }
+
+        private void show_Click(object sender, EventArgs e)
+        {
+            {
+                string item = comboBoxItem.Text;
+                int quantity = Convert.ToInt32(textBoxQuantity.Text);
+                double price = 0;
+
+
+                if (item == "Math")
+                {
+                    price = 120 * quantity;
+                }
+                else if (item == "English")
+                {
+                    price = 100 * quantity;
+                }
+                else if (item == "Bangla")
+                {
+                    price = 80 * quantity;
+                }
+                else if (item == "Art")
+                {
+                    price = 90 * quantity;
+                }
+
+
+                else
+                {
+                    MessageBox.Show("Select an item");
+                }
+
+                richTextBoxShow.Text = "Name :" + textBoxName.Text + "\n" + "Contact No: " + textBoxContract.Text + "\n" +
+                                       "Address :" + textBoxAddress.Text + "\n" + "Item : " + item + "\n" + "Price :" + price;
+
+
+
+
+            }
+        }
+
+        private void comboBoxItem_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void coffeShop_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
